dec11400e4653164603802683f0cd324febd00f3
YuqinHu <h878688736@gmail.com>
2023-02-07 19:33:15 -0800

    eqArray

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
+const eqArrays  = function(actual, expected){
+  for (let i = 0; i < actual.length; i++){
+    if (!assertEqual(actual[i],expected[i])){
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
