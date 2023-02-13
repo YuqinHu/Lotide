c9e3be5c11ab100b680aa8f8fff9f5b4a94fb41a
YuqinHu <h878688736@gmail.com>
2023-02-13 15:40:10 -0800

    refactor middle

--- middle.js
+++ middle.js
@@ -1,30 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-
-const eqArrays  = function(actual, expected) {
-  for (let i = 0; i < actual.length; i++) {
-    if (!assertEqual(actual[i],expected[i])) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual  = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
-
 const middle = function(array) {
   let middle = [];
   if (array.length < 3) {
@@ -40,9 +13,4 @@ const middle = function(array) {
 };
 
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
