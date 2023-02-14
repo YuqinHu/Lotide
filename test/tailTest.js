b370eb05618dfc8c34931f25f37384ff9f3efda1
YuqinHu <h878688736@gmail.com>
2023-02-13 18:04:07 -0800

    refactor tail and head

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,22 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
 
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+describe("#tail", () => {
+  it("returns 2 for ['Hello', 'Lighthouse', 'Labs'] lenghth", () => {
+    assert.strictEqual(result.length, 2); ;
+  });
+
+  it("returns 'Lighthouse' for result[0]", () => {
+    assert.strictEqual(result[0], "Lighthouse"); 
+  });
+
+  it("returns 'Labs' for result[1]", () => {
+    assert.strictEqual(result[1], "Labs"); 
+  });
+});
